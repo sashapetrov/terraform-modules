6c15724e047aedd5baec019c3557de13939bf477
Aleksandr Petrov <poker07@yandex.ru>
2021-05-30 18:48:53 +0700

    aws_securitygroup added

---
+++ aws_securitygroup/main.tf
@@ -0,0 +1,22 @@
+# Security group for our network
+
+resource "aws_security_group" "network_sg" {
+  name        = "network_sg"
+  description = "Network security_group for ports: ${var.port_list[*]}"
+  dynamic ingress {
+    for_each var.port_list
+    content {
+    from_port = ingress.value
+    to_port = ingress.value
+    protocol = "tcp"
+    cidr_blocks = var.cidr_list
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  }
+
+}
---
+++ aws_securitygroup/variables.tf
@@ -0,0 +1,7 @@
+variable "port_list" {
+  default = ["22", "80", "443"]
+}
+
+variable "cidr_list" {
+  default = ["0.0.0.0/0"]
+}
