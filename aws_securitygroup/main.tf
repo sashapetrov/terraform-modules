b64341c5ebe13e1589196f6c4216d4a3aa02ce34
Aleksandr Petrov <poker07@yandex.ru>
2021-05-30 18:55:58 +0700

    fixes in securitygroup

--- aws_securitygroup/main.tf
+++ aws_securitygroup/main.tf
@@ -11,12 +11,11 @@ resource "aws_security_group" "network_sg" {
       protocol    = "tcp"
       cidr_blocks = var.cidr_list
     }
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = -1
-      cidr_blocks = ["0.0.0.0/0"]
-    }
   }
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
